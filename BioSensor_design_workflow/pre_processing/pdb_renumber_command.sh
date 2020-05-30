957069f4f9521998a5076a8cd5f4cf46ccb87da9
Gerald-li <65849841+Gerald-li@users.noreply.github.com>
2020-05-30 23:22:08 +0800

    Add files via upload

---
+++ BioSensor_design_workflow/pre_processing/pdb_renumber_command.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+## The script of 'PDB_renumber.py' from the network, if there is copyright infringement, please contact us within 24 hours to delete.
+# original website: https://github.com/AspirinCode/protein-science/tree/master/scripts-and-tools/renumber_pdb
+python PDB_renumber.py -i 1D6N_A68K_G102K_F35L.pdb -r > 1D6N_A68K_G102K_F35L_renumber.pdb
